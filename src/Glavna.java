a4cbc869cc988541f676b1bc39c7ebce1275f30d
david.elim45 <davidelim170204@gmail.com>
2024-01-23 17:53:10 +0100

    Konstruktor

---
+++ src/Glavna.java
@@ -0,0 +1,13 @@
+
+public class Glavna {
+
+	public static void main(String[] args) {
+		Kvadar kv = new Kvadar();
+		System.out.println("Volumen"+kv.getVolumen());
+		
+		Kvadar kv2 = new Kvadar (2,2,2);
+		System.out.println("Volumen_2" + kv.getVolumen());
+
+	}
+
+}
